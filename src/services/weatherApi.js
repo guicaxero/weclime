8ac734c49a83a6278ee5dab376058a035ff7db38
guicaxero <caxeropl@gmail.com>
2025-04-15 16:18:31 -0300

    fix: Alterando a requisição da API para https

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -4,7 +4,7 @@ export async function requestApi(city) {
     const API_KEY = '48bfdb31f6e440ffb55112831251504'
     
     try {
-        const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&lang=pt`)
+        const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&lang=pt`)
         const data = await res.json()
         console.log('passou por aqui', data)
         return data;
